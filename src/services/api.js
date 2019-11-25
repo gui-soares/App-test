55183ed4dca590b30d90e26dad9288aad5b91cdd
gui-soares <eng.guilhermemsoares@gmail.com>
2019-11-25 00:33:54 -0300

    fix: axios requests

--- src/services/api.js
+++ src/services/api.js
@@ -1,38 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.github.com/',
+  baseURL: 'http://api.github.com/',
 });
-
-api.defaults.headers.post['Content-Type'] = 'application/json';
-
-api.interceptors.request.use(config => {
-  console.tron.log('axios request: ', {
-    url: config.url,
-    data: config.data,
-  });
-
-  return config;
-});
-
-api.interceptors.response.use(
-  res => {
-    console.tron.log('axios response success: ', {
-      status: res.status,
-      data: res.data,
-    });
-
-    return res;
-  },
-  err => {
-    console.tron.log('axios response error: ', {
-      err,
-      status: err.response ? err.response.status : 'none',
-      data: err.response ? err.response.data : 'none',
-    });
-
-    return Promise.reject(err);
-  }
-);
 
 export default api;
